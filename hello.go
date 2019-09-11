9ab772324a47f795f9a972db775d3309ee6b3d1c
AAA <924370970@qq.com>
2019-09-11 11:24:17 +0800

    码云

--- hello.go
+++ hello.go
@@ -6,6 +6,6 @@ func main() {
 	fmt.Println("Hello World1!")
 	fmt.Println("Hello World2!")
 	fmt.Println("Hello World3!")
-	fmt.Println("Love You")
+	fmt.Println("码云！！！")
 	fmt.Println("YYYYYYY")
 }
